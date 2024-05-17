cea8b63700b543a876db24e843b84447408ec52c
nafih <nafihkbl123@gmail.com>
2024-05-17 06:55:18 -0700

    create signup api route

---
+++ api/controllers/auth.controllers.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signupController = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    res.status(400).json({ maessage: "all fields are required" });
+  }
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("signup succesfull");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  // console.log(req.body);
+};
