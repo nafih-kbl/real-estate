651f0248a1ca5bf09c3a07846dc2dd6c57a1e90c
nafih <nafihkbl123@gmail.com>
2024-05-23 12:13:50 -0700

    sign in page functionality

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -39,7 +39,7 @@ export const signinController=async(req,res,next)=>{
   if(!validPassword) return next(errorHandler(401,'wrong credentials'));
   const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
   const {password:pass,...rest}=validUser._doc;
-  res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+  res.cookie('access_token',token,{httpOnly:true,sameSite: 'none', secure: true}).status(200).json(rest);
   } catch (error) {
     next(error)
   }
